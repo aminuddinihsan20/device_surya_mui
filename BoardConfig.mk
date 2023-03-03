6ffb4b393ded6780d62acb2c726f93fccddaa6c3
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    surya: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,9 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
+# Lineage Health
+TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
+
 # Media
 TARGET_USES_ION := true
 TARGET_DISABLED_UBWC := true
