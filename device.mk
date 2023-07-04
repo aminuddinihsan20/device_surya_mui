e21cb35808a6492ecbc7626111218b0161399198
rdxzv <rdxzv.dev@gmail.com>
2023-07-04 07:16:55 +0000

    surya: import XiaomiParts
    - from https://github.com/rd-stuffs/android_device_xiaomi_surya/
    - commit history https://github.com/rd-stuffs/android_device_xiaomi_surya/commits/topaz-wip/parts
    
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- device.mk
+++ device.mk
@@ -20,6 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Inherit XiaomiParts
+$(call inherit-product, device/xiaomi/surya/parts/parts.mk)
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
@@ -106,9 +109,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+USE_DEX2OAT_DEBUG := false
 
 # Display
 PRODUCT_PACKAGES += \
