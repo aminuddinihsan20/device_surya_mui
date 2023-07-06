05f73838414b70ff53aed94a334b8c3d9a982534
rdxzv <rdxzv.dev@gmail.com>
2023-07-06 04:35:03 +0000

    surya: Import few perf configs
    - from https://github.com/rd-stuffs/android_device_xiaomi_surya/tree/topaz-wip
    - commit history https://github.com/rd-stuffs/android_device_xiaomi_surya/commits/topaz-wip/configs/perf
    
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- device.mk
+++ device.mk
@@ -347,6 +347,10 @@ PRODUCT_PACKAGES += \
 
 # Properties
 include $(LOCAL_PATH)/vendor_logtag.mk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
 # Power
 PRODUCT_PACKAGES += \
