ea51839d5cbdd1e366819a361ffedfeda70078e1
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-07-17 10:29:10 +0800

    surya : add vendor logtags

---
+++ vendor_logtag.mk
@@ -0,0 +1,9 @@
+ifeq (eng,$(TARGET_BUILD_VARIANT))
+VENDOR_LOG_LEVEL=I
+else
+VENDOR_LOG_LEVEL=S
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.log.tag.OpenGLRenderer=$(VENDOR_LOG_LEVEL) \
+persist.log.tag.ContrastColorUtil=$(VENDOR_LOG_LEVEL)
