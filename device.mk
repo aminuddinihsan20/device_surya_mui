82e5ec028f92a080f34692d06c79c52484ba2504
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-07-17 09:21:29 +0800

    surya : add support miui camera

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Camera
+$(call inherit-product-if-exists, device/xiaomi/surya-miuicamera/config.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
