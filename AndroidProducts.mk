0eb6a00d3045fde1d3d1ad4dc18777e762eb6650
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-07-19 08:36:03 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_surya.mk
+    $(LOCAL_DIR)/awaken_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_surya-user \
-    superior_surya-userdebug \
-    superior_surya-eng
+    awaken_surya-user \
+    awaken_surya-userdebug \
+    awaken_surya-eng
