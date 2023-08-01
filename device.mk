8547331afaaad66cec79e80859f43d6bbecb5864
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-08-01 09:38:29 +0800

    surya : build with bcr call recorder

--- device.mk
+++ device.mk
@@ -71,6 +71,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Basic Call Recorder
+$(call inherit-product, vendor/bcr/bcr.mk)
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
