0b555018ca8464e9f66c312842f5c0b9c08555f5
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-08-01 09:36:09 +0800

    surya : droidx initial bring up

--- droidx_surya.mk
+++ droidx_surya.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/droidx/config/common.mk)
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-DROIDX_GAPPS := true
+DROIDX_GAPPS := false
 TARGET_ENABLE_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
