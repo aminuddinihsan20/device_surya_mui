68f46c407c55b7d05f97b81899fecdf25ac9b9ce
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-08-01 09:36:09 +0800

    surya : droidx initial bring up

--- droidx_surya.mk
+++ droidx_surya.mk
@@ -11,10 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DroidX stuff.
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+DROIDX_GAPPS := false
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := false
 
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := droidx_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
