b477e571467f6a6dd7bd1d31508dc597850bb06d
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-08-04 23:29:10 +0800

    surya : initial bringup for project elixir

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -12,12 +12,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BUILD_APERTURE_CAMERA := true
-ALPHA_MAINTAINER := Muichiro
-TARGET_ENABLE_BLUR := true
+ELIXIR_BUILD_TYPE := UNOFFICIAL
+ELIXIR_MAINTAINER := Muichiro
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_SUPPORTS_QUICK_TAP := false
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := false
 
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
