17266fc3ed137d3a75eb35183edbb0741f4005db
Muichiro09 <secretvirtue123@gmail.com>
2023-08-07 12:53:01 +0800

    surya : Voltage-ify

--- voltage_surya.mk
+++ voltage_surya.mk
@@ -12,9 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_surya
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := voltage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
