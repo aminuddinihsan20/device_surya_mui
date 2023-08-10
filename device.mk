a49800ba754b8c3fa678e22d264bcc6d72ae0521
Muichiro09 <secretvirtue123@gmail.com>
2023-08-10 11:59:36 +0800

    surya: Remove Xiaomi Parts"
    This reverts commit 3f5c410bae2019dc47f081072a4f113fbfa0e05f.

--- device.mk
+++ device.mk
@@ -20,9 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
-# Inherit XiaomiParts
-$(call inherit-product, device/xiaomi/surya/parts/parts.mk)
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
@@ -108,6 +105,10 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     libpiex_shim
+
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
 
 # Dex/ART optimization
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
