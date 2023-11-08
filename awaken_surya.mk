6605271e1b8a3a252d56fcf119bfe56e25d9a970
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-11-08 08:47:39 +0800

    surya : Awaken!

--- awaken_surya.mk
+++ awaken_surya.mk
@@ -12,9 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := awaken_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
