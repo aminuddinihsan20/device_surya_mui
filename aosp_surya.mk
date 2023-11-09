9754a5296a8cd6a9f29cab656905e9cfddaa6be9
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-11-09 22:29:30 +0800

    surya : AOSP

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -12,12 +12,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_MATLOG := true
 TARGET_USES_MIUI_CAMERA := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_ENABLE_BLUR := true
+IS_PHONE := true
+TARGET_BUILD_APERTURE_CAMERA := false
 
-PRODUCT_NAME := awaken_surya
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
